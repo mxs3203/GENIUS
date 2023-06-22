14a8ebb94ae27289d9bb0f2a69feec07ebdc8bca
mxs3203 <mxs3203@g.rit.edu>
2023-06-22 19:18:59 +0200

    num of workers = 0

--- Training/train.py
+++ Training/train.py
@@ -53,7 +53,7 @@ dataset = TCGAImageLoader(config['meta_data'],
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-train_size = int(len(dataset) * 0.75)
+train_size = int(len(dataset) * 0.6)
 test_size = len(dataset) - train_size
 print("Train size: ", train_size)
 print("Test size: ", test_size)
